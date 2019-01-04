9962b5815ec5fbe086fb44912dbfde2a95e4de93
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-01-04 19:55:03 +0400

    finish

--- bot.js
+++ bot.js
@@ -1,36 +1,30 @@
 const Discord = require('discord.js');
+const jsonfile = require('jsonfile')
+
 const client = new Discord.Client();
 
 const serverid = "530704221834575873" // ID - сервер в который все идет
 const serverid2 = "530702782764679168" // ID2 - сервер с которого парсится
 
-const channelIds = [
-	{
-		name: 'канал-1',
-		idChannelTarget: '530702782764679170',
-		idChannelMy: '530769504515522560'
-	},
-	{
-		name: 'канал-2',
-		idChannelTarget: '530703009454358528',
-		idChannelMy: '530721320674263040'
-	},
-	{
-		name: 'канал-3',
-		idChannelTarget: '530760972051415040',
-		idChannelMy: '530721342207557673'
-	}
-];
+var channelIds = [];
+
+// Читаем JSON файл с описанием каналов
+jsonfile.readFile('channelsList.json', function (err, obj) {
+	if (err) console.error(err)
+	channelIds = obj
+})
 
 client.on("message", message => {
 
 	//если сообщение с сервера, с которого идет парсинг
 	if (message.guild.id == serverid2) {
 
+	// находим корректный объект
 	var currentChanel = channelIds.find((el) => {
 		return el.idChannelTarget === message.channel.id
 	});
 
+	// Если объект не пустой то делаем всё по схеме
 	if (currentChanel !== undefined) {
 		//выполнять, если есть embed
 		if (message.embeds.toString() != "") {
