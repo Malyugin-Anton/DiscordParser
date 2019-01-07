aa74fa2621cdbe7db9bf19fc5f249e5726cbc0e8
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-01-08 00:29:44 +0400

    done configuration file for first akkaunt

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
+// Аккаунт 1
+// token1
+// Аккаунт с которого будем пиздить
+// serverId1 - сервер с которого нужно пиздить
+// serverId2 - промежуточный сервер для проброса сообщений
+// serverId3: ,
+// serverId4: 
+
 module.exports = {
-  serverId1: 123,
-  serverId2: 123,
-  serverId3: 123,
-  serverId4: 123
+  token1: 'NTE4ODU0Mzc5MTk4MDg3MTkx.DxSXUw.5HiQ7i5oeENGq_KXpf2QoO68R08',
+  token2: 'mfa.CyeKJgElx6pK7_dmYqAPu-DJcJ9epBHnokVQEHLGP7l709NoWIffgbgby0tw4YMo4R-bLbiGT8PaWKriS4d6',
+  serverId1: 379317079720853514,
+  serverId2: 531743897135874088
 };
