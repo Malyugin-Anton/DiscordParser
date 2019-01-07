68ee80f794de3a34b92c41d29df8f27203039c8d
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-01-07 11:21:10 +0400

    script for send messages

---
+++ bot-send.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const serverid = "530704221834575873" // ID - сервер в который все идет
+const serverid2 = "530702782764679168" // ID2 - сервер с которого парсится
+
+client.on("message", message => {
+  //если сообщение с сервера, с которого идет парсинг
+  if (message.guild.id == serverid2) {
+
+    //выполнять, если есть embed
+    if (message.embeds.toString() != "") {
+      if (message.content.startsWith("http")) {
+
+        client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(message.content)
+      
+      } else {
+
+        if (message.content.includes("http")) {
+          
+          client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(message.content)
+        
+        } else {
+          
+          var embed = new Discord.MessageEmbed(message.embeds[0])
+          
+          if (message.content == "") {
+            
+            client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(embed)
+          
+          } else {
+            
+            client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(message.content)
+            client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(embed)
+
+          }
+        }
+      }
+    } else {
+
+      client.guilds.get(serverid).channels.get(client.guilds.get(serverid).channels.find('name', message.channel.name).id).send(message.content)
+
+    }
+  }
+
+})
+
+client.login(process.env.BOT_TOKEN); //вход для бота
