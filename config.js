48c05844508b9c3021c0a8ef7d9ac10b5a241b6f
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-03-20 00:03:19 +0400

    change token for akk 1 and akk 2

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
 // ID серверов должны быть типа строка
 
 module.exports = {
-  token1: 'NTE4ODU0Mzc5MTk4MDg3MTkx.DxZnzw.BjKy31ZmXcwzG3SvoMXmkVvzizY',
-  token2: 'NTA5ODAyMjc0NjU1MTc0NjU2.DxZrPA.o8Kj9lvj5wUAIgJ6R8UHtrH8qJI',
+  token1: 'NTE4ODU0Mzc5MTk4MDg3MTkx.D3LZOg.A3ea8HEvbE63V-QcOutXqvP7Nv4',
+  token2: 'NTA5ODAyMjc0NjU1MTc0NjU2.D3LcDA.Nnaagv0T1TRcX4DwDkrTN_cYVEU',
   serverIdPasr1: "379317079720853514",
   serverIdClone1: "531743897135874088",
   serverIdPasr2: "531743897135874088",
