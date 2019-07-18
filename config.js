fa34267fbbc5cb305243f008b77a2316e0e6d6e9
Malyugin-Anton <ya.anton.m22@gmail.com>
2019-07-19 02:42:01 +0400

    Remove unnecessary

--- config.js
+++ config.js
@@ -6,10 +6,10 @@
 // ID серверов должны быть типа строка
 
 module.exports = {
-  token1: 'NTE4ODU0Mzc5MTk4MDg3MTkx.D3LZOg.A3ea8HEvbE63V-QcOutXqvP7Nv4',
-  token2: 'NTA5ODAyMjc0NjU1MTc0NjU2.D3LcDA.Nnaagv0T1TRcX4DwDkrTN_cYVEU',
-  serverIdPasr1: "379317079720853514",
-  serverIdClone1: "531743897135874088",
-  serverIdPasr2: "531743897135874088",
-  serverIdClone2: "523258763256266752"
+  token1: 'NTE4ODU0Mzc5MTk4MDg3MTkx.D3LZOg.A3ea8HEvbE63V-QcOutXqadsajl',
+  token2: 'NTA5ODAyMjc0NjU1MTc0NjU2.D3LcDA.Nnaagv0T1ASDasdasdAS_sdasds',
+  serverIdPasr1: "379317079720800000",
+  serverIdClone1: "531743897135800000",
+  serverIdPasr2: "531743897135800000",
+  serverIdClone2: "523258763256200000"
 };
